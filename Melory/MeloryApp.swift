84a152b00920a53fe7105ec3b6ba06c1d6066e42
Manuel Thomsen <masotoma@hotmail.com>
2023-05-19 23:22:46 +0200

    Initial Commit

---
+++ Melory/MeloryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeloryApp.swift
+//  Melory
+//
+//  Created by Manuel Thomsen on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeloryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
