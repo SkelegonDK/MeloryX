3b11b33eb31b387c76ac74eb7e9b2d343f52fb16
Manuel Thomsen <masotoma@hotmail.com>
2023-07-15 22:11:36 +0200

    updated sound samples

--- Melory/GameView.swift
+++ Melory/GameView.swift
@@ -24,7 +24,42 @@ struct GameView: View {
         "HARDCHORD": 8
     ]
     let notesPerRow = 4
-    let notes = ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4"]
+    let toneURLs = [
+        "C3" = URL(fileURLWithPath: Bundle.main.path(forResource: "samples/C3", ofType: "wav")!),
+        "C#3" = URL(fileURLWithPath: Bundle.main.path(forResource: "C#3", ofType: "wav")!),
+        "D3" = URL(fileURLWithPath: Bundle.main.path(forResource: "D3", ofType: "wav")!),
+        "D#3" = URL(fileURLWithPath: Bundle.main.path(forResource: "D#3", ofType: "wav")!),
+        "E3" = URL(fileURLWithPath: Bundle.main.path(forResource: "E3", ofType: "wav")!),
+        "F3" = URL(fileURLWithPath: Bundle.main.path(forResource: "F3", ofType: "wav")!),
+        "F#3" = URL(fileURLWithPath: Bundle.main.path(forResource: "F#3", ofType: "wav")!),
+        "G3" = URL(fileURLWithPath: Bundle.main.path(forResource: "G3", ofType: "wav")!),
+        "G#3" = URL(fileURLWithPath: Bundle.main.path(forResource: "G#3", ofType: "wav")!),
+        "A3" = URL(fileURLWithPath: Bundle.main.path(forResource: "A3", ofType: "wav")!),
+        "A4" = URL(fileURLWithPath: Bundle.main.path(forResource: "A4", ofType: "wav")!),
+        "A5" = URL(fileURLWithPath: Bundle.main.path(forResource: "A5", ofType: "wav")!),
+        "A#4" = URL(fileURLWithPath: Bundle.main.path(forResource: "A#4", ofType: "wav")!),
+        "B4" = URL(fileURLWithPath: Bundle.main.path(forResource: "B4", ofType: "wav")!),
+        "B5" = URL(fileURLWithPath: Bundle.main.path(forResource: "B5", ofType: "wav")!),
+        "C4" = URL(fileURLWithPath: Bundle.main.path(forResource: "C4", ofType: "wav")!),
+        "D4" = URL(fileURLWithPath: Bundle.main.path(forResource: "D4", ofType: "wav")!),
+        "D5" = URL(fileURLWithPath: Bundle.main.path(forResource: "D5", ofType: "wav")!),
+        "D#4" = URL(fileURLWithPath: Bundle.main.path(forResource: "D#4", ofType: "wav")!),
+        "E4" = URL(fileURLWithPath: Bundle.main.path(forResource: "E4", ofType: "wav")!),
+        "E5" = URL(fileURLWithPath: Bundle.main.path(forResource: "E5", ofType: "wav")!),
+        "F4" = URL(fileURLWithPath: Bundle.main.path(forResource: "F4", ofType: "wav")!),
+        "F5" = URL(fileURLWithPath: Bundle.main.path(forResource: "F5", ofType: "wav")!),
+        "F#3" = URL(fileURLWithPath: Bundle.main.path(forResource: "F#3", ofType: "wav")!),
+        "G3" = URL(fileURLWithPath: Bundle.main.path(forResource: "G3", ofType: "wav")!),
+        "G4" = URL(fileURLWithPath: Bundle.main.path(forResource: "G4", ofType: "wav")!),
+        "G5" = URL(fileURLWithPath: Bundle.main.path(forResource: "G5", ofType: "wav")!),
+        "G#3" = URL(fileURLWithPath: Bundle.main.path(forResource: "G#3", ofType: "wav")!),
+
+    ]
+
+    let numCards = totalSquares / 2
+    var toneIndices = Array(0..<numCards)
+    toneIndices += toneIndices
+    toneIndices.shuffle()
 
     var body: some View {
         VStack {
